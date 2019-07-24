714d3ddf2838ee76c1f2945a178a4cdcd740a009
abogacki <andrzej.bogacki@intive.com>
2019-07-24 09:32:16 +0200

    Adds babel config to backend, adds mock weather api response to save free api calls

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
-const axios = require('axios')
+import express from 'express'
+import http from 'http'
+import socketIo from 'socket.io'
+import axios from 'axios'
+import index from './routes/index'
+import 'dotenv/config'
 
 const port = process.env.PORT || 4001
-const index = require('./routes/index')
 
 const app = express()
 app.use(index)
+
 const server = http.createServer(app)
 
 const io = socketIo(server)
@@ -19,19 +21,30 @@ io.on('connect', socket => {
     clearInterval(interval)
   }
   console.log('Client connected')
-  interval = setInterval(() => getApiAndEmit(socket), 10000)
+  interval = setInterval(() => getApiAndEmit(socket), 5000)
 
   socket.on('disconnect', () => {
     console.log('Client disconenct')
   })
 })
 
-const darkSkyUrl =
-  'https://api.darksky.net/forecast/004e080b7d25e328f7d2f97c96c7d7ea/43.7695,11.2558'
+const darkSkyUrl = 'https://api.darksky.net/forecast//43.7695,11.2558'
+
+const getDarkSkyUrl = (latitude, longitude) => {
+  const baseUrl = 'https://api.darksky.net/forecast'
+  const url = `${baseUrl}/${process.env.DARK_SKY_KEY}/${latitude},${longitude}`
+  return url
+}
 
 const getApiAndEmit = async socket => {
   try {
-    const res = await axios.get(darkSkyUrl)
+    // console.log(import);
+    // const res = await axios.get(darkSkyUrl)
+    const data = await import('./_mocks/weatherResponse.json')
+    const res = {
+      data,
+    }
+
     socket.emit('FromAPI', res.data)
   } catch (error) {
     console.error(`Error: ${error.code}; ${error.message}`)
